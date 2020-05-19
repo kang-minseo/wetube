9e63790d6eae027c6f344502ab2be64d58c46fd8
Kang minseo <kangms9185@gmail.com>
2020-05-19 22:12:51 +0900

    ~2.22 mixins, controller

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Wetube
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] join
+- [x] Login
+- [x] Search
+- [ ] user Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:123123,
+        title : 'Video awesome',
+        description : 'This is something',
+        views : 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators : {
+            id: 12312,
+            name:"minseo",
+            email:"minseo@lasd.com"
+        }
+    },
+    {
+        id:43434,
+        title : 'Video perfect',
+        description : 'This is something',
+        views : 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators : {
+            id: 12312,
+            name:"minseo",
+            email:"minseo@lasd.com"
+        }
+    },
+    {
+        id:12376757123,
+        title : 'Video nice',
+        description : 'This is something',
+        views : 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators : {
+            id: 12312,
+            name:"minseo",
+            email:"minseo@lasd.com"
+        }
+    },
+    {
+        id:99999,
+        title : 'Video nice',
+        description : 'This is something',
+        views : 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creators : {
+            id: 12312,
+            name:"minseo",
+            email:"minseo@lasd.com"
+        }
+    }
+]
